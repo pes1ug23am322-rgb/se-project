070e0440ff1d6ee8210bf28c0d95fdc0f0c52b85
Suhas <pes1ug23am322@pesu.pes.edu>
2025-11-17 01:30:58 +0530

    BTPC-4 Docs: Comment explaining PDF kit usage

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// BTPC-4: Generating PDF using PDFKit
 // BTPC-2: Calling fetchBlog service here
 // BTPC-1: Handles receiving blog URL from user
 // Added documentation comments for clarity – commit improvement
